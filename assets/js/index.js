72228992c63053ae487f439077f3dad5807a0b8e
Saurabh Chaudhary <86178655+saurav0416@users.noreply.github.com>
2021-07-14 12:09:05 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+function bmiCalculator() {
+  n = document.getElementById("name").value;
+  w = document.getElementById("weight").value;
+  h = document.getElementById("height").value;
+  bmi = parseFloat(w / (h * h)).toFixed(2);
+}
+
+    function showResult(){
+    document.querySelector(".main-heading").style.fontSize="3.5rem";
+    output = document.querySelector(".main-section").innerHTML = `<p
+        style="
+        font-family:sans-serif;
+        color:red; 
+        font-size:1.5rem; 
+        font-weight: bold; 
+        margin-top: 10%;
+        padding:10px;
+        margin-left:200px;
+        margin-right:200px;
+    ">   
+        ${n} your BMI is ${bmi}
+    </p>`;
+
+    document.querySelector(".go-back").style.visibility="visible";
+}
+
+
+
+
+
+
+
+
